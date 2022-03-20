aa5b2fea99d53458433bcffbe21bee811c94a7d8
coder-qi <zhangfengxian-test>
2022-03-21 07:24:05 +0800

    chapter7

---
+++ ch07/petstore/src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    <div class="navbar navbar-default">
+      <div class="navbar-header">
+        <h1>
+          <router-link :to="{ name: 'iMain' }">
+            {{ sitename }}
+          </router-link>
+        </h1>
+      </div>
+      <div class="nav navbar-nav navbar-right cart">
+        <button
+          type="button"
+          class="btn btn-default btn-lg"
+          v-on:click="showCheckout"
+        >
+          <span class="glyphicon glyphicon-shopping-cart">
+            {{ cartItemCount }}</span
+          >
+          Checkout
+        </button>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: "my-header",
+  data() {
+    return {
+      sitename: "Vue.js Pet Depot",
+    };
+  },
+  props: ["cartItemCount"],
+  methods: {
+    showCheckout() {
+      this.$router.push({ name: "Form" });
+    },
+  },
+};
+</script>
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
